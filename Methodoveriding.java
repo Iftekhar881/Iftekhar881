7c827c4d4e7a06dcb6b8a6d267dc7db6463e62a8
Iftekhar881 <114158574+Iftekhar881@users.noreply.github.com>
2022-10-01 12:27:50 +0530

    Add files via upload

---
+++ Methodoveriding.java
@@ -0,0 +1,10 @@
+package CoreJAva;
+
+public class Methodoveriding {
+		   //Method's of supper class
+			void Display() {
+				System.out.println("I am from supper class Method_Overiding");
+			}
+
+		}
+
